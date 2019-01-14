86d551e558c206e8dfacbd99c0e9ef96e88da8ac
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-01-14 23:28:45 +0700

    ыфв

--- animals/src/main/java/ru/ibusewinner/animals/PiarGC.java
+++ animals/src/main/java/ru/ibusewinner/animals/PiarGC.java
@@ -1,13 +1,14 @@
 package ru.ibusewinner.animals;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.bukkit.Bukkit;
 
 public class PiarGC {
-	private int timer = 0;
+	private int time = 0;
 	
+	private static ArrayList<String> messages;
+	public static int timer = 0;
 	private String VK = "§7[§c!§7]§3 Наша группа §9ВК§3: §1https://vk.com/zhiznzhivotnyh";
 	private String site = "§7[§c!§7]§3 Наш сайт: §dhttps://vk.cc/8V2iL9";
 	//private String forum = "§7[§c!§7]§3 Наш форум: §awww.forum.скоро.net";
@@ -15,34 +16,43 @@ public class PiarGC {
 	private String donate = "§7[§c!§7]§3 Поддержать наш проект можно по ссылке: §bhttps://vk.cc/8V2iL9";
 	private String donate_cmd = "§7[§c!§7]§3 Включить бустер можно через: §e/donate";
 	
+	private int msgI = 0;
+	
+	public PiarGC() 
+	{
+		messages = new ArrayList<String>();
+		messages.add("§7[§c!§7]§3 Наша группа §9ВК§3: §1https://vk.com/zhiznzhivotnyh");
+		messages.add("§7[§c!§7]§3 Наш сайт: §dhttps://vk.cc/8V2iL9");
+		//messages.add("§7[§c!§7]§3 Наш форум: §awww.forum.скоро.net)";
+		messages.add("§7[§c!§7]§3 Наш дискорд: §9https://discord.gg/DDTDFgD");
+		messages.add("§7[§c!§7]§3 Поддержать наш проект можно по ссылке: §bhttps://vk.cc/8V2iL9");
+		messages.add("§7[§c!§7]§3 Включить бустер можно через: §e/donate");
+	}
+	
 	@SuppressWarnings("deprecation")
 	public void time() {
 		timer = Bukkit.getScheduler().scheduleAsyncRepeatingTask(MainAnimals.plugin,new Runnable() {
 			
 			@Override
 			public void run() {
-				if(timer == 1) {
-					Bukkit.broadcastMessage(VK);
-					timer++;
-				}else if(timer == 2) {
-					Bukkit.broadcastMessage(site);
-					timer++;
-				}else if(timer == 3) {
-					//Bukkit.broadcastMessage(forum);
-					//timer++;
-				//}else if(timer == 4) {
-					Bukkit.broadcastMessage(discord);
-					timer++;
-				}else if(timer == 4) {
-					Bukkit.broadcastMessage(donate);
-					timer++;
-				}else if(timer == 5) {
-					Bukkit.broadcastMessage(donate_cmd);
-					timer++;
-				}else {
-					timer++;
-				}
-				timer++;
+				if (msgI >= messages.size()-1)
+					msgI = 0;
+				
+				
+				if (time % 1 == 0) // Каждую одну минуту будет посылать сообщение 
+					Bukkit.broadcastMessage(messages.get(msgI));
+
+//				if (time % 2 == 0) // Каждые две минуты будет посылать сообщение 
+//					Bukkit.broadcastMessage(messages.get(msgI));
+				
+				
+//				if (time % 5 == 0) // Каждые пять минут будет посылать сообщение 
+//				Bukkit.broadcastMessage(messages.get(msgI));
+//				...
+//				etc
+				
+				msgI++;
+				time++;
 			}
 		},0,20*60);
 	}
