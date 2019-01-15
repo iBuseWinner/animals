e351dc06309e413d3e770cca4f686c0c65eb6ed2
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-01-15 23:20:32 +0700

    New scoreboard system

--- animals/src/main/java/ru/ibusewinner/animals/PiarGC.java
+++ animals/src/main/java/ru/ibusewinner/animals/PiarGC.java
@@ -9,12 +9,6 @@ public class PiarGC {
 	
 	private static ArrayList<String> messages;
 	public static int timer = 0;
-	private String VK = "§7[§c!§7]§3 Наша группа §9ВК§3: §1https://vk.com/zhiznzhivotnyh";
-	private String site = "§7[§c!§7]§3 Наш сайт: §dhttps://vk.cc/8V2iL9";
-	//private String forum = "§7[§c!§7]§3 Наш форум: §awww.forum.скоро.net";
-	private String discord = "§7[§c!§7]§3 Наш дискорд: §9https://discord.gg/DDTDFgD";
-	private String donate = "§7[§c!§7]§3 Поддержать наш проект можно по ссылке: §bhttps://vk.cc/8V2iL9";
-	private String donate_cmd = "§7[§c!§7]§3 Включить бустер можно через: §e/donate";
 	
 	private int msgI = 0;
 	
@@ -37,9 +31,6 @@ public class PiarGC {
 			public void run() {
 				if (msgI >= messages.size()-1)
 					msgI = 0;
-				
-				
-				if (time % 1 == 0) // Каждую одну минуту будет посылать сообщение 
 					Bukkit.broadcastMessage(messages.get(msgI));
 
 //				if (time % 2 == 0) // Каждые две минуты будет посылать сообщение 
