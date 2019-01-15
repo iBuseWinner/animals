f27c98ab4ec1aad2f15598a283fe8c43a2cc956e
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2019-01-15 19:16:02 +0300

    упдэйт
    Ну, ненужная фигня <:D

--- animals/src/main/java/ru/ibusewinner/animals/SBAnimals.java
+++ animals/src/main/java/ru/ibusewinner/animals/SBAnimals.java
@@ -1,4 +1,4 @@
-	package ru.ibusewinner.animals;
+package ru.ibusewinner.animals;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,113 +41,75 @@ public class SBAnimals implements Listener{
 				int bal = APIAnimals.getBal(player);
 				
 				Score score = objective.getScore("§aБаланс: "+"§7"+String.valueOf(bal)+"р");
-				score.setScore(15);
+				score.setScore(10);
 				
 				int income = APIAnimals.getIncome(player);
 				
 				Score score2 = objective.getScore("§aДоход: "+"§6"+String.valueOf(income)+"р/сек §bx"+APIAnimals.getLocalBoost(player) * Booostoor.agboostm * Booostoor.gboostm);
-				score2.setScore(14);
+				score2.setScore(9);
 				
 				Score score3 = objective.getScore("§0");
-				score3.setScore(13);
-				
-				int respawns = APIAnimals.getRespawns(player);
-				
-				Score score4 = objective.getScore("§aПерерождений: "+"§1"+String.valueOf(respawns));
-				score4.setScore(12);
+				score3.setScore(8);
 				
 				Score score6 = objective.getScore("§aЖивотное:");
-				score6.setScore(11);
+				score6.setScore(7);
 				
 				if(APIAnimals.getAnimal(player) == 0) {
 					String type = "§8Не выбрано";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 1) {
 					String type = "§eкот";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 2) {
 					String type = "§fсобака";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 3) {
 					String type = "§6корова";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 4) {
 					String type = "§dсвинья";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 5) {
 					String type = "§fкурица";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 7) {
 					String type = "§fмедведь";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 8) {
 					String type = "§6кролик";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}else if(APIAnimals.getAnimal(player) == 9) {
 					String type = "§9попугай";
 					Score score7 = objective.getScore(type);
-					score7.setScore(10);
+					score7.setScore(6);
 				}
 				
 				int level = APIAnimals.getLevel(player);
 				
 				Score score14 = objective.getScore("§aУровень: "+"§9"+String.valueOf(level));
-				score14.setScore(9);
+				score14.setScore(5);
 				
 				Score score9 = objective.getScore("§1");
-				score9.setScore(8);
+				score9.setScore(4);
 				
 				int hearts = APIAnimals.getHearts(player);
 				
 				Score score8 = objective.getScore("§cСердечек§a: "+"§c"+String.valueOf(hearts)+"§l❤");
-				score8.setScore(7);
+				score8.setScore(3);
 				
-				int humans = APIAnimals.getHumans(player);
-				
-				Score score10 = objective.getScore("§aХозяев: "+"§2"+String.valueOf(humans));
-				score10.setScore(6);
-				
-				int bizs = APIAnimals.getBizs(player);
-				
-				Score score12 = objective.getScore("§aБизнесов: "+"§e"+String.valueOf(bizs));
-				score12.setScore(5);
-				
-				Score score13 = objective.getScore("§5");
-				score13.setScore(4);
-				
-				int boost = APIAnimals.getLocalBoost(player);
-				
-				int m = 0;
-				int h = 0;
-				Score score15 = objective.getScore("§aБустер: §3x"+String.valueOf(boost));
-				if (MainAnimals.boosters.containsKey(player))
-				{
-					int currentTime = MainAnimals.boosters.get(player).currentTime;
-					int boostTimer = MainAnimals.boosters.get(player).boostTimer;
-					int difference = boostTimer - currentTime;
-					m = difference / 60 + 1;
-					
-					h = m / 60;
-					m = m - h*60;
-					score15 = objective.getScore("§aБустер: §3x"+String.valueOf(boost) + " §7на §3" + h + ":" + m);
-				}
-				score15.setScore(3);
-				
-				Score score16 = objective.getScore("§3");
-				score16.setScore(2);
+				Score score13 = objective.getScore("§5");
+				score13.setScore(2);
 				
 				Score score19 = objective.getScore("§aОнлайн: §a§l " + Bukkit.getOnlinePlayers().size());
 				score19.setScore(1);
-				
-				
 				
 				player.setScoreboard(board);
 
